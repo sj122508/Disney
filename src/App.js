a795c38cff8f851e371a6507390d9cd815f9588d
James Viojan <james.viojan@accenture.com>
2022-01-22 23:49:09 +0800

    Add Home and Carousel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-
+// 3:49 https://www.youtube.com/watch?v=0mVbNp1ol_w
 function App() {
   return (
     <div >
